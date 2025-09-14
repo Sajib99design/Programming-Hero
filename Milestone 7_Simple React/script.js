e2fdb81bbfa0e7c0b52f27c3283d5d8187e56beb
Sajib99design <sajib99design@gmail.com>
2025-09-14 19:18:29 +0600

    react simple

---
+++ Milestone 7_Simple React/script.js
@@ -0,0 +1,27 @@
+const isActive = false;
+
+const showUser = () => {
+    console.log('show user green')
+}
+
+const hideUser = () => {
+    console.log('hide user dark')
+}
+
+
+// step -1
+// if (isActive) {
+//     showUser()
+// }
+// else {
+//     hideUser()
+// }
+
+
+// step -2
+// isActive ? showUser() : hideUser ();
+// let test = true ? showUser() : hideUser ();
+
+// isActive && showUser();    //true hole output
+
+isActive || hideUser()   //false hole output
